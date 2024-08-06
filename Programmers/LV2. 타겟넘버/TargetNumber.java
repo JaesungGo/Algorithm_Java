e8eddefe4d08274d435f8f6a3eb80d419ad2bec1
JaesungGo <102339979+JaesungGo@users.noreply.github.com>
2024-08-06 09:37:09 +0900

    LV2. 타겟넘버
    LV2. 타겟넘버

---
+++ Programmers/LV2. 타겟넘버/TargetNumber.java
@@ -0,0 +1,27 @@
+class solution{
+    int answer;
+    int[] numbers;
+    int target;
+
+    public int solution(int[] numbers, int target){
+        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
+
+        dfs(0,0);
+        return answer;
+    }
+
+    public void dfs(int index, int sum){
+        // 탈출 조건
+        if(index == numbers.length){
+            if(sum == target) answer++;
+            return;
+        }
+
+
+        // 수행 동작
+        dfs(index+1, sum+numbers[index]);
+        dfs(index+1, sum-numbers[index]);
+    }
+}
